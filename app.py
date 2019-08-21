7ee70dd44d4c94f6d25adc90e8edabdb262ec3af
Moutasem Akkad <moutasemalakkad@gmail.com>
2019-08-21 19:46:59 -0400

    modified py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
 
 
