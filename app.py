b3e42d371af5db9b0ff00d13bc2f1139707d6a4b
Mer Arnel Manahan <mermanahan@gmail.com>
2019-08-27 11:36:03 -0400

    updated app.py

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey, inspect
 from sqlalchemy.orm import Session, Mapper
 from sqlalchemy import create_engine
 import numpy as np
+<<<<<<< HEAD
+=======
+import calendar
+
+
+# <<<<<<< HEA
+>>>>>>> 1ef6fa7d... modified app.py to have both endpoints working
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, Flask,url_for, send_from_directory, send_file, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -34,13 +41,77 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/api/gas-leaks")
-def gasLeaks():
+@app.route("/api/zip-leaks")
+def zipLeaks():
     ## Use pandas to read csv
     df = pd.read_csv("final_leaks.csv")
 
     ## Convert the Date column to correct datetime format
     df['Date'] = pd.to_datetime(df['Date'], format = '%Y-%m-%d')
+
+    leaks_dict = {}
+    for i in range(6):
+        wh_year = 2013+i
+        year_data = df[df['Date'].dt.year == wh_year]
+        
+        ## get data for leaks per zipcode for that given year
+        year_zip_data = pd.DataFrame(year_data.groupby('ZIP_CODE').sum()['TMAX'])
+        year_zip_data.rename(columns = {'TMAX': 'Total_Leaks'}, inplace=True)
+    #     print(i)
+    #     print(year_zip_data.head())
+        year_zip_data.reset_index(inplace=True)
+        json = year_zip_data.to_json(orient='records')
+        leaks_dict[wh_year] = json
+
+    return jsonify(leaks_dict)
+
+##### Route for the monthly leaks
+
+@app.route("/api/monthly-leaks")
+def monthLeaks():
+
+        ## Use pandas to read csv
+    df = pd.read_csv("final_leaks.csv")
+
+    ## Convert the Date column to correct datetime format
+    df['Date'] = pd.to_datetime(df['Date'], format = '%Y-%m-%d')
+
+    months = pd.DataFrame(df['Date'].dt.month)
+    months.rename(columns = {"Date": 'Month'}, inplace=True)
+
+    df['Month'] = df['Date'].dt.month
+    df['Year'] = df['Date'].dt.year
+
+    df['Month'] = df['Month'].apply(lambda x: calendar.month_abbr[x])
+    grouped_month_day_df  = df.groupby(['Year','Month', 'Date'], sort=False).count()['TMAX']
+
+    grouped_month_day_df = pd.DataFrame(grouped_month_day_df)
+    grouped_month_day_df.rename(columns = {'TMAX': 'Leak_Count'}, inplace =  True)
+
+    grouped_month_day_indexed_df = grouped_month_day_df.reset_index()
+    grouped_month_day_indexed_df['Date'] = grouped_month_day_indexed_df['Date'].astype(str)
+
+    response = {}
+    for row in grouped_month_day_indexed_df.values:
+    
+        year = row[0]
+        month = row[1]
+        date = row[2]
+        leak_c = row[3]
+        
+        
+        if year not in response:
+            response[year] = {}
+        else:
+            if month not in response[year]:
+                response[year][month] = {}
+            else:
+                if date not in response[year][month]:
+                    response[year][month][date] = leak_c
+                else:
+                    pass
+
+    return jsonify(response)
 
     # ## get the data for the full year where date = 2013
     # year_data = df[df['Date'].dt.year == 2013]
@@ -81,6 +152,7 @@ def gasLeaks():
     # final_monthly_leaks_dict = monthly_leaks_dict['Number_of_Leaks']
 
 
+<<<<<<< HEAD
     # # Place the dictionaries from above into final dictionary to respond to API request
     # final_dict = {
     #     "ZipData:" : year_data_dict,
@@ -103,6 +175,53 @@ def gasLeaks():
         leaks_dict[wh_year] = json
 
     return jsonify(leaks_dict)
+=======
+
+
+#login
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
+
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(80))
+
+# class LoginForm(FlaskForm):
+#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+#     remember = BooleanField('remember me')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+
+
+@app.route('/logged-out')
+# @login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+>>>>>>> 1ef6fa7d... modified app.py to have both endpoints working
 
 
 @app.route('/static/<path:path>')
